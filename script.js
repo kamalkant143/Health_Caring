f2d59293eee1b98fd3bc1700963b044581559f4f
kamal kant <kamalshakya14311@gmail.com>
2024-11-30 16:23:47 +0530

    health-care

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-const slideBox =document.querySelectorAll(".care-box")
-const slider =document.querySelectorAll(".button")
+const slideBox = document.querySelectorAll(".care-box");
+const slider = document.querySelectorAll(".button");
 
+const insideDiv = document.createElement("div");
+insideDiv.className = "button";
+const insideDivTwo = document.createElement("div");
+insideDivTwo.className = "button";
 
-    const insideDiv =document.createElement('div')
-    insideDiv.className="button"
-    const insideDivTwo =document.createElement('div')
-    insideDivTwo.className="button"
+slider.appendChild(insideDiv);
+slider.appendChild(insideDivTwo);
 
-
-    slider.appendChild(insideDiv)
-    slider.appendChild(insideDivTwo)
-
-
-    slider.addEventListener("click",function(){
-      slideBox.forEach(function(){
-        console.log(slideBox)
-      })
-    })
-      
-
+slider.addEventListener("click", function () {
+  slideBox.forEach(function () {
+    console.log(slideBox);
+  });
+});
