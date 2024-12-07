7a5812436cf6d9e9b5ea49568ae620027ab70d90
kamal kant <kamalshakya14311@gmail.com>
2024-12-07 14:36:01 +0530

    health-care

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const slideBox = document.querySelectorAll(".care-box");
-const slider = document.querySelectorAll(".button");
+// const slideBox = document.querySelectorAll(".care-box");
+// const slider = document.querySelectorAll(".button");
 
-const insideDiv = document.createElement("div");
-insideDiv.className = "button";
-const insideDivTwo = document.createElement("div");
-insideDivTwo.className = "button";
+// const insideDiv = document.createElement("div");
+// insideDiv.className = "button";
+// const insideDivTwo = document.createElement("div");
+// insideDivTwo.className = "button";
 
-slider.appendChild(insideDiv);
-slider.appendChild(insideDivTwo);
+// slider.appendChild(insideDiv);
+// slider.appendChild(insideDivTwo);
 
-slider.addEventListener("click", function () {
-  slideBox.forEach(function () {
-    console.log(slideBox);
-  });
-});
+// slider.addEventListener("click", function () {
+//   slideBox.forEach(function () {
+//     console.log(slideBox);
+//   });
+// });
 
+// window.addEventListener("scroll",function(){
+//   let navBar=document.querySelector(".nav-wrapper")
+//   navBar.classList.toggle("fixed",window.scrollY > 0)
+// })
+
+let navBar=document.querySelector(".nav-wrapper")
+console.log(navBar);
+
+window.addEventListener("scroll",()=>{
+  navBar.classList.toggle("fixed" ,window.scrollY > 100)
+}
+
+)
