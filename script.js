6fe3617b4bfd2b6ecae4dbd5deac4669f6c15ba2
kamal kant <kamalshakya14311@gmail.com>
2025-01-05 16:37:39 +0530

    new changing

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-// const slideBox = document.querySelectorAll(".care-box");
-// const slider = document.querySelectorAll(".button");
+const hamBurgar =document.querySelector(".hemburgar")
+const navbarList=document.querySelector(".nav-bar-list")
 
-// const insideDiv = document.createElement("div");
-// insideDiv.className = "button";
-// const insideDivTwo = document.createElement("div");
-// insideDivTwo.className = "button";
-
-// slider.appendChild(insideDiv);
-// slider.appendChild(insideDivTwo);
-
-// slider.addEventListener("click", function () {
-//   slideBox.forEach(function () {
-//     console.log(slideBox);
-//   });
-// });
-
-// window.addEventListener("scroll",function(){
-//   let navBar=document.querySelector(".nav-wrapper")
-//   navBar.classList.toggle("fixed",window.scrollY > 0)
-// })
-
+ 
 let navBar=document.querySelector(".nav-wrapper")
-console.log(navBar);
 
 window.addEventListener("scroll",()=>{
-  navBar.classList.toggle("fixed" ,window.scrollY > 100)
+  navBar.classList.toggle("fixed" ,window.scrollY > 50)
 }
 
 )
+
+hamBurgar.addEventListener("click",()=>{
+  navbarList.classList.toggle("navout")
+})
