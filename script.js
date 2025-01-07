627721bb228b6360e97920fbc0797e7d63ce6b88
kamal kant <kamalshakya14311@gmail.com>
2025-01-07 17:14:52 +0530

    new changing

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const hamBurgar = document.querySelector(".hemburgar");
 const navbarList = document.querySelector(".nav-bar-list");
-
 const navBar = document.querySelector(".nav-wrapper");
+const apBtn =document.querySelector(".apm-btn")
+const popUp =document.querySelector("#appointment-popup")
+apBtn.addEventListener("click",()=>{
+   popUp.classList.remove("ap-hidden")
+})
 
 window.addEventListener("scroll", () => {
   navBar.classList.toggle("fixed", window.scrollY > 50);
