b77b0a467ced7a403eca142ee442ff7bf7e225f7
kamal kant <kamalshakya14311@gmail.com>
2025-01-12 20:54:52 +0530

    new changing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamBurgar = document.querySelector(".hemburgar");
 const navbarList = document.querySelector(".nav-bar-list");
 const navBar = document.querySelector(".nav-wrapper");
 const apBtn =document.querySelector(".apm-btn")
+const apBtnMobile =document.querySelector(".apm-btn-moblie")
 const popUp =document.querySelector("#appointment-popup")
 const noScroll =document.querySelector("body")
 const popClose =document.querySelector(".fa-xmark")
@@ -30,8 +31,13 @@ apBtn.addEventListener("click",()=>{
   popUp.classList.remove("ap-hidden")
  noScroll.classList.add("no-scroll")
 })
+apBtnMobile.addEventListener("click",()=>{
+  popUp.classList.remove("ap-hidden")
+ noScroll.classList.add("no-scroll")
+})
 
 popClose.addEventListener("click",()=>{
  popUp.classList.add("ap-hidden")
 noScroll.classList.remove("no-scroll")
 })
+
